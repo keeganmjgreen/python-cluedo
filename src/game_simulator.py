d3143b1e57b4b25eb330e884f5930715d4580f5f
Keegan Green <keeganmjgreen@gmail.com>
2026-04-06 18:04:32 -0400

    refactor(game_simulator): use agent objects themselves rather than indices where possible

--- src/game_simulator.py
+++ src/game_simulator.py
@@ -29,28 +29,42 @@ class GameSetup:
     extra_cards: Sequence[RumorCard]
     agents: dict[AgentIndex, BaseAgent]
 
+    @property
+    def players(self) -> dict[AgentIndex, BasePlayer]:
+        return {
+            agent.agent_index: agent
+            for agent in self.agents.values()
+            if isinstance(agent, BasePlayer)
+        }
+
+    @property
+    def observers(self) -> dict[AgentIndex, BaseObserver]:
+        return {
+            agent.agent_index: agent
+            for agent in self.agents.values()
+            if isinstance(agent, BaseObserver) and not isinstance(agent, BasePlayer)
+        }
+
 
 def run_turn(
     turn_index: int,
-    agents: dict[AgentIndex, BaseAgent],
+    players: dict[AgentIndex, BasePlayer],
     current_player_index: AgentIndex,
+    observers: dict[AgentIndex, BaseObserver],
 ) -> None:
     print(f"Turn: {turn_index}.")
-    agent_indices = list(agents.keys())
-    player_indices = [
-        index for index, player in agents.items() if isinstance(player, BasePlayer)
-    ]
-    current_player = agents[current_player_index]
+    agents: dict[AgentIndex, BaseAgent] = {**players, **observers}
+    current_player = players[current_player_index]
     guess = current_player.make_guess(turn_index=turn_index)
-    for agent_index in agent_indices:
-        agents[agent_index].add_game_log_entry(
+    for agent in agents.values():
+        agent.add_game_log_entry(
             turn_index=turn_index,
             turn_player_index=current_player.agent_index,
             guess=guess,
             card_reveals=[],
         )
     furthest_player_reached = False
-    n_other_players = len(player_indices) - 1
+    n_other_players = len(players) - 1
     for direction in [-1, +1]:
         players_pos_delta = 1
         while players_pos_delta <= n_other_players // 2 + 1:
@@ -64,22 +78,19 @@ def run_turn(
             second_player_pos = (
                 current_player.agent_index + players_pos_delta * direction
             )
-            second_player = agents[
-                (len(player_indices) + second_player_pos) % len(player_indices)
-            ]
+            second_player = players[(len(players) + second_player_pos) % len(players)]
             rumor_card = second_player.answer_guess(guess)
             current_player.shown_card(
                 turn_index=turn_index,
                 other_player_index=second_player.agent_index,
                 rumor_card=rumor_card,
             )
-            for third_agent_index in [
-                agent_index
-                for agent_index in agent_indices
-                if agent_index
+            for third_agent in [
+                agent
+                for agent in agents.values()
+                if agent.agent_index
                 not in [current_player.agent_index, second_player.agent_index]
             ]:
-                third_agent = agents[third_agent_index]
                 third_agent.shown_card(
                     turn_index=turn_index,
                     other_player_index=second_player.agent_index,
@@ -92,30 +103,23 @@ def run_turn(
 
 
 def run_game(
-    game_setup: GameSetup, artifacting_id: int | None, reveal_extra_cards_first: bool
+    setup: GameSetup, artifacting_id: int | None, reveal_extra_cards_first: bool
 ) -> None:
-    agents = game_setup.agents
-    agent_indices = list(agents.keys())
-    player_indices = [
-        index for index, player in agents.items() if isinstance(player, BasePlayer)
-    ]
-    observer_indices = [index for index in agents.keys() if index not in player_indices]
     if artifacting_id is not None:
         probabilities_artifact_mgr = ProbabilitiesArtifactManager(start_over=False)
-    n_extra_cards = len(game_setup.extra_cards)
+    n_extra_cards = len(setup.extra_cards)
     turn_index = 0
-    for agent_index in agent_indices:
-        agents[agent_index].add_game_log_entry(turn_index=turn_index, card_reveals=[])
+    for agent in setup.agents.values():
+        agent.add_game_log_entry(turn_index=turn_index, card_reveals=[])
         if n_extra_cards != 0 and reveal_extra_cards_first:
-            agents[agent_index].shown_extra_cards(
-                turn_index=turn_index, rumor_cards=game_setup.extra_cards
+            agent.shown_extra_cards(
+                turn_index=turn_index, rumor_cards=setup.extra_cards
             )
     won_agent_indices: list[AgentIndex] = []
     while True:
-        for current_player_index in player_indices:
+        for player in setup.players.values():
             if artifacting_id is not None:
-                for agent_index in agent_indices:
-                    agent = agents[agent_index]
+                for agent in setup.agents:
                     if not isinstance(agent, SmartBotObserver):
                         continue
                     probabilities_ser = agent.solve_truths_cnf_probabilities(
@@ -124,50 +128,50 @@ def run_game(
                     probabilities_artifact_mgr.append_probabilities_ser(
                         artifacting_id=artifacting_id,
                         agent_type=(
-                            "Player"
-                            if isinstance(agents[agent_index], BasePlayer)
-                            else "Observer"
+                            "Player" if isinstance(agent, BasePlayer) else "Observer"
                         ),
-                        agent_index=agent_index,
+                        agent_index=agent.agent_index,
                         turn_index=turn_index,
                         probabilities_ser=probabilities_ser,
                     )
             turn_index += 1
-            run_turn(turn_index, agents, current_player_index)
+            run_turn(turn_index, setup.players, player.agent_index, setup.observers)
             newly_won_agent_indices: list[AgentIndex] = []
-            for agent_index in agent_indices:
-                if agent_index in won_agent_indices:
+            for agent in setup.agents.values():
+                if agent.agent_index in won_agent_indices:
                     continue
-                agent = agents[agent_index]
                 if n_extra_cards != 0 and not reveal_extra_cards_first:
                     if isinstance(agent, SmartBotObserver):
                         agent_must_see_extra_cards = agent.must_see_extra_cards(
                             turn_index=turn_index
                         )
                         if agent_must_see_extra_cards:
-                            agents[agent_index].shown_extra_cards(
-                                turn_index=turn_index,
-                                rumor_cards=game_setup.extra_cards,
+                            agent.shown_extra_cards(
+                                turn_index=turn_index, rumor_cards=setup.extra_cards
                             )
                 result = agent.try_solving_crime()
                 if result is not None:
-                    won_agent_indices.append(agent_index)
-                    newly_won_agent_indices.append(agent_index)
+                    won_agent_indices.append(agent.agent_index)
+                    newly_won_agent_indices.append(agent.agent_index)
             newly_won_player_indices = [
-                index for index in newly_won_agent_indices if index in player_indices
+                index
+                for index in newly_won_agent_indices
+                if index in setup.players.keys()
             ]
             if len(newly_won_player_indices) > 0:
                 print(
                     f"The following players have solved the crime in the last turn: {newly_won_player_indices}"
                 )
             newly_won_observer_indices = [
-                index for index in newly_won_agent_indices if index in observer_indices
+                index
+                for index in newly_won_agent_indices
+                if index in setup.observers.keys()
             ]
             if len(newly_won_observer_indices) > 0:
                 print(
                     f"The following observers have solved the crime in the last turn: {newly_won_observer_indices}"
                 )
-            if len(won_agent_indices) == len(agent_indices):
+            if len(won_agent_indices) == len(setup.agents):
                 print("By now, all players and observers have solved the crime.")
                 return
 
@@ -223,7 +227,7 @@ def game_simulator(
 ) -> None:
     game_setup = set_up_game(player_types=player_types, observer_types=observer_types)
     run_game(
-        game_setup=game_setup,
+        setup=game_setup,
         artifacting_id=artifacting_id,
         reveal_extra_cards_first=reveal_extra_cards_first,
     )
