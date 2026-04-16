3e2db42f4bd1d070ea1189d252ef104ba7219b4e
Keegan Green <keeganmjgreen@gmail.com>
2026-04-15 21:46:35 -0400

    further improve dashboard
    - Show "Start the game to see probabilities" on first open.
    - Remove Turn # padding.
    - Set probability colorbar range to [0, 1].
    - Set figure font to match dashboard font.

--- src/common/dashboard.py
+++ src/common/dashboard.py
@@ -2,10 +2,14 @@ import logging
 import threading
 from time import sleep
 
+import pandas as pd
 import plotly.express as px
 from dash import Dash, Input, Output, dcc, html
 
 from common import store
+from common.agent_utils import CASE_FILE
+from common.cards import RUMORS
+from common.consts import EXTRA_CARDS
 from common.store import (
     AGENT,
     APPROX_PROBABILITY,
@@ -16,6 +20,7 @@ from common.store import (
 
 logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
+FONT_FAMILY = "Helvetica Neue, Helvetica, Arial, sans-serif"
 UPDATE_FREQ_MS = 200
 
 
@@ -35,7 +40,7 @@ app.layout = html.Div(
         dcc.Graph(id="graph-content"),
         dcc.Interval(id="interval-component", interval=UPDATE_FREQ_MS, n_intervals=0),
     ],
-    style={"fontFamily": "Helvetica Neue, Helvetica, Arial, sans-serif"},
+    style={"fontFamily": FONT_FAMILY},
 )
 
 
@@ -68,27 +73,32 @@ def update_options(n, search_value, agent):
 )
 def update_probabilities_heatmap(n: int, agent: str, turn_index: int):
     df = store.get_probabilities_df()
-    if len(df) == 0:
-        return None
-    if agent in ["(Latest)", None]:
-        agent = df[AGENT].iloc[-1]
-    df = df[df[AGENT] == agent]
-    if turn_index in ["(Latest)", None]:
-        turn_index = df[TURN_INDEX].iloc[-1]
-    probabilities_ser = (
-        df.set_index([AGENT, TURN_INDEX])
-        .loc[agent, turn_index]
-        .set_index([CARD_LOCATION, RUMOR_CARD])[APPROX_PROBABILITY]
+    if len(df) > 0:
+        if agent in ["(Latest)", None]:
+            agent = df[AGENT].iloc[-1]
+        df = df[df[AGENT] == agent]
+        if turn_index in ["(Latest)", None]:
+            turn_index = df[TURN_INDEX].iloc[-1]
+        probabilities_ser = (
+            df.set_index([AGENT, TURN_INDEX])
+            .loc[agent, turn_index]
+            .set_index([CARD_LOCATION, RUMOR_CARD])[APPROX_PROBABILITY]
+        )
+        probability_df = probabilities_ser.unstack()
+        title = f"Approximate Probabilities | Turn {turn_index}, Bot {agent}"
+    else:
+        probability_df = pd.DataFrame(index=[CASE_FILE, EXTRA_CARDS])
+        title = "Start the game to see probabilities."
+    probability_df = probability_df.reindex(
+        columns=[str(r) for r in RUMORS], fill_value=0
     )
-    probability_df = probabilities_ser.unstack()
-    fig = px.imshow(
-        probability_df.rename_axis(index="Card Location", columns="Rumor Card")
+    probability_df = probability_df.rename_axis(
+        index="Card Location", columns="Rumor Card"
     )
+    fig = px.imshow(probability_df, range_color=(0, 1))
     fig.update_xaxes(type="category")
     fig.update_yaxes(type="category")
-    fig.update_layout(
-        title=f"Approximate Probabilities | Turn {turn_index:2.0f}, Bot {agent}"
-    )
+    fig.update_layout(title=title, font_family=FONT_FAMILY)
     return fig
 
 
